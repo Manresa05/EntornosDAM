1d9501d66a5827deb9f08fb4fbacf6e6af71ba8e
dam23-11 <dam23-11@c16x07.iescierva.net>
2024-01-30 09:27:42 +0100

    EDT2 Mostrar Nombre y Comandos

--- ejemplo.java
+++ ejemplo.java
@@ -1,6 +1,8 @@
 public class Ejemplo{
     public static void main(String[] args){
-        System.out.println("HelloWord!");    
+        System.out.println("HelloWord!"); 
+        String nombre = "Pablo";
+        System.out.println("Mi nombre es " + nombre);   
     }
 
 }
